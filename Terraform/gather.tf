bd6fb475865fba9da23a13fda3dd8dc3cba3704b
AmanPathak-DevOps <aman07pathak@gmail.com>
2025-10-02 19:25:20 +0530

    formatted the code, and terraform action improved

--- Terraform/gather.tf
+++ Terraform/gather.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name = "name"
     values = [
       "ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*",
       "ubuntu/images/hvm/ubuntu-noble-24.04-amd64-server-*",
