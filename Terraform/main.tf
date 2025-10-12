02d61000bf26a8434ec05029d3ce39533e1a7cb8
AmanPathak-DevOps <aman07pathak@gmail.com>
2025-10-12 22:13:43 +0530

    fix: add sg reference

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "ec2" {
   subnet_id            = aws_subnet.public-subnet[count.index].id
   instance_type        = var.ec2_instance_type
   iam_instance_profile = aws_iam_instance_profile.iam-instance-profile.name
-  security_groups      = [aws_security_group.default-ec2-sg.arn]
+  security_groups      = [aws_security_group.default-ec2-sg.id]
   root_block_device {
     volume_size = var.ec2_volume_size
     volume_type = var.ec2_volume_type
